e7e9c68844a5e5693e279d0421a917e951541c16
root <root@bjyz-sys-gpu-kongming9.bjyz.baidu.com>
2022-11-18 11:48:13 +0000

    fix encoding to utf-8 in open()

--- deploy/ppcv/ops/models/classification/postprocess.py
+++ deploy/ppcv/ops/models/classification/postprocess.py
@@ -1,15 +1,15 @@
-# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved. 
-#   
-# Licensed under the Apache License, Version 2.0 (the "License");   
-# you may not use this file except in compliance with the License.  
-# You may obtain a copy of the License at   
-#   
-#     http://www.apache.org/licenses/LICENSE-2.0    
-#   
-# Unless required by applicable law or agreed to in writing, software   
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-# See the License for the specific language governing permissions and   
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 import os
@@ -28,7 +28,7 @@ def parse_class_id_map(class_id_map_file):
 
     try:
         class_id_map = {}
-        with open(class_id_map_file, "r") as fin:
+        with open(class_id_map_file, "r", encoding='utf-8') as fin:
             lines = fin.readlines()
             for line in lines:
                 partition = line.split("\n")[0].partition(" ")
@@ -40,6 +40,7 @@ def parse_class_id_map(class_id_map_file):
 
 
 class Topk(object):
+
     def __init__(self, topk=1, class_id_map_file=None):
         assert isinstance(topk, (int, ))
         self.class_id_map = parse_class_id_map(class_id_map_file)
@@ -57,13 +58,12 @@ class Topk(object):
                 score_list.append(probs[i].item())
                 if self.class_id_map is not None:
                     label_name_list.append(self.class_id_map[i.item()])
-            
+
             label_name = label_name_list
-            
+
             result = {
                 output_keys[0]: clas_id_list,
-                output_keys[1]: np.around(
-                    score_list, decimals=5).tolist(),
+                output_keys[1]: np.around(score_list, decimals=5).tolist(),
                 output_keys[2]: label_name
             }
             y.append(result)
@@ -71,7 +71,11 @@ class Topk(object):
 
 
 class ThreshOutput(object):
-    def __init__(self, threshold, default_label_index=0, class_id_map_file=None):
+
+    def __init__(self,
+                 threshold,
+                 default_label_index=0,
+                 class_id_map_file=None):
         self.threshold = threshold
         self.default_label_index = default_label_index
         self.class_id_map = parse_class_id_map(class_id_map_file)
@@ -82,14 +86,19 @@ class ThreshOutput(object):
             index = probs.argsort(axis=0)[::-1].astype("int32")
             top1_id = index[0]
             top1_score = probs[top1_id]
-            
+
             if top1_score > self.threshold:
                 rtn_id = top1_id
             else:
                 rtn_id = self.default_label_index
-            
-            label_name = self.class_id_map[rtn_id] if self.class_id_map is not None else ""
 
-            result = {output_keys[0]: rtn_id, output_keys[1]: probs[rtn_id], output_keys[2]: label_name}
+            label_name = self.class_id_map[
+                rtn_id] if self.class_id_map is not None else ""
+
+            result = {
+                output_keys[0]: rtn_id,
+                output_keys[1]: probs[rtn_id],
+                output_keys[2]: label_name
+            }
             y.append(result)
         return y
